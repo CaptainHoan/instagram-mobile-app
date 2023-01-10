c08830cb6f639b4c302a7cdef11fabe7b8d4dc59
captain <hoancaptain123456789@gmail.com>
2023-01-10 15:41:40 +0700

    add like function

--- src/screens/MainScreen/HomeScreen.tsx
+++ src/screens/MainScreen/HomeScreen.tsx
@@ -1,10 +1,10 @@
 import { View, Text, SafeAreaView, Image, TouchableOpacity, ScrollView } from 'react-native'
 import React, { useEffect, useState } from 'react'
 import { FontAwesome5 } from '@expo/vector-icons';
-import { Timestamp, addDoc, collection, doc, onSnapshot, orderBy, query, serverTimestamp, setDoc, where } from 'firebase/firestore';
+import { collection, doc, onSnapshot, orderBy, query, serverTimestamp, setDoc, where } from 'firebase/firestore';
 import { auth, db } from '../../../firebase';
-import { AUTH_USER_PROFILE, POST_TYPE } from '../../types/currentUserType';
-import { Fontisto, Feather, AntDesign   } from '@expo/vector-icons';
+import { POST_TYPE } from '../../types/currentUserType';
+import { Feather, AntDesign   } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native';
 
 const HomeScreen = () => {
@@ -14,21 +14,7 @@ const HomeScreen = () => {
   if(currentUser === null) return 
 
   const navigation = useNavigation()
-  const [loggedInUser, setLoggedInUser] = useState<AUTH_USER_PROFILE[]>([])
   const [posts, setPosts] = useState<POST_TYPE[]>([])
-  const [isLiked, setIsLiked] = useState<boolean>(false)
-  const [postLiked, setPostLiked] = useState<string>('')
-
-  //fetch current user infos
-  useEffect(() => 
-    onSnapshot(collection(db, 'users'), (snapshot) => {
-      const profile = snapshot.docs.filter(doc => doc.id === currentUser.uid).map(doc => ({
-        id: doc.id,
-        ...doc.data()
-      }))
-      setLoggedInUser(profile);
-    })  
-,[])
 
   //fetch posts
   useEffect(() => 
@@ -41,32 +27,7 @@ const HomeScreen = () => {
     })
   ,[db])
 
-  //fetch Liked
-  useEffect(() => 
-    onSnapshot(collection(db, 'posts', 'liked'), (snapshot) => {
-      const liked = snapshot.docs.map(doc => ({
-        id: doc.id,
-        ...doc.data()
-      }))
-      console.log('liked is', liked)
-      //setIsLiked(liked.liked)
-    })
-  ,[db])
-
-  //console.log('posts are',posts)
-  //handle when post is liked
-  const handleLikeFunc = (post: POST_TYPE) => {
-    setIsLiked(true)
-    setPostLiked(post.id)
-    if(isLiked === false) {
-      addDoc(collection(db, 'posts', post.id, 'liked'), {
-        userWhoLiked: currentUser.uid,
-        liked: true,
-        timestamp: serverTimestamp()
-      })
-    }
-    if(isLiked === true) return
-  }
+  console.log(posts)
 
   return (
     <SafeAreaView className='flex-1'>
@@ -113,21 +74,11 @@ const HomeScreen = () => {
               </View>
 
               {/**likes, comments icon */}
-              <View className='flex-row items-center space-x-3 mx-4'>
-                <TouchableOpacity onPress={() => handleLikeFunc(post)}>
-                  <AntDesign 
-                    name="heart" 
-                    size={24} 
-                    color={isLiked === true && postLiked === post.id ? "red" : 'gray'} 
-                  />
-                </TouchableOpacity>
-                <TouchableOpacity onPress={() => navigation.navigate('Comment')}>
-                  <Feather name="message-circle" size={24} color="black" />
-                </TouchableOpacity>
-                <TouchableOpacity>
-                  <Feather name="send" size={24} color="black" />
-                </TouchableOpacity>
-              </View>
+              <PostFooter 
+                navigation={navigation}
+                post={post}
+                currentUser = {currentUser}
+              />
 
               {/**post Bio */}
               <View className='mx-4'>
@@ -147,6 +98,73 @@ const HomeScreen = () => {
         }
       </ScrollView>
     </SafeAreaView>
+  )
+}
+
+const PostFooter = ({ post, navigation, currentUser}: any) => {
+
+  const [isLiked, setIsLiked] = useState<boolean | null>(null)
+  const [postLiked, setPostLiked] = useState<string[]>([])
+
+  //if (postLiked === null) return 
+
+  //fetch likes of each post
+  useEffect(() => 
+    onSnapshot(query(collection(db, 'posts', post.id, 'liked'), where('liked', '==', true)), (snapshot) => {
+      const likes = snapshot.docs.map(doc => ({
+        id: doc.id,
+        ...doc.data()
+      }))
+      console.log('likes is', likes)
+      setIsLiked(likes[0]?.liked)
+      const postLiked = snapshot.docs.map(doc => (
+        doc.id
+      ))
+      setPostLiked(postLiked)
+      console.log('postLiked is',postLiked)
+    })
+  ,[isLiked])
+
+  console.log(isLiked)
+
+  //handle when post is liked
+  const handleLikeFunc = (post: POST_TYPE) => {
+    setDoc(doc(db, 'posts', post.id, 'liked', currentUser.uid), {
+      userWhoLiked: currentUser.uid,
+      liked: true,
+      timestamp: serverTimestamp()
+    })
+  }
+
+  return (
+    <View>
+      <View className='flex-row items-center space-x-3 mx-4 mt-3'>
+        <TouchableOpacity onPress={() => handleLikeFunc(post)}>
+          <AntDesign 
+            name="heart" 
+            size={24} 
+            color={isLiked === true && postLiked?.includes(currentUser.uid) ? "red" : 'gray'} 
+          />
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => navigation.navigate('Comment')}>
+          <Feather name="message-circle" size={24} color="black" />
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Feather name="send" size={24} color="black" />
+        </TouchableOpacity>
+      </View>
+      {
+        postLiked.length !== 0
+        ? (
+          <View className='mx-4 mt-2'>
+            <Text className='font-semibold'>{postLiked?.length > 0 && postLiked.length} {postLiked.length > 1 ? 'likes' : 'like'}</Text>
+          </View>
+        )
+        : null
+      }
+      
+      
+    </View>
   )
 }
 
